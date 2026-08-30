ec3cc73f8356cd2af7c7c40b31bfe44cfa66adf4
oBoolt <bernardo.patto.pinheiro@gmail.com>
2026-08-30 00:44:19 -0300

    chore!: change name of project
    comply with the guidelines for plugins by removing "obs" prefix from the file created
    
    https://obsproject.com/forum/threads/forum-resource-and-ip-policy.178569/
    Resource Branding section 2

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 #include "shortcuts.h"
 
 OBS_DECLARE_MODULE()
-OBS_MODULE_USE_DEFAULT_LOCALE("obs-global-shortcuts", "en-US")
+OBS_MODULE_USE_DEFAULT_LOCALE("linux-global-shortcuts", "en-US")
 
 const char *obs_module_name() { return "GlobalShortcuts"; }
 const char *obs_module_description() { return obs_module_text("Description"); }
